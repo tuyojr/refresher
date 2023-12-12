00fd98ecae014e6852d4b26d21adeaef137c0ff1
tuyojr <olutuyod@gmail.com>
2023-12-12 14:59:20 +0100

    come let's play rock, paper, scissors

--- 04_randomisation_and_lists/rock_paper_scissors.py
+++ 04_randomisation_and_lists/rock_paper_scissors.py
@@ -30,19 +30,22 @@ scissors = '''
 
 # printing player choices
 game_images = [rock, paper, scissors]
-print(game_images[user_choice])
-computer_choice = random.randint(0, 2)
-print(f"Computer chose: {computer_choice}")
-print(game_images[computer_choice])
-
-# game rules and logic
-if user_choice == 2 and computer_choice == 1:
-    print("You win!")
-elif user_choice == 1 and computer_choice == 0:
-    print("You win!")
-elif user_choice == 0 and computer_choice == 2:
-    print("You win!")
-elif user_choice == computer_choice:
-    print("It's a draw.")
+if user_choice > 3 or user_choice < 0:
+    print("You typed an invalid number, you lose.")
 else:
-    print("You lose.")
+    print(game_images[user_choice])
+    computer_choice = random.randint(0, 2)
+    print(f"Computer chose: {computer_choice}")
+    print(game_images[computer_choice])
+
+    # game rules and logic
+    if user_choice == 2 and computer_choice == 1:
+        print("You win!")
+    elif user_choice == 1 and computer_choice == 0:
+        print("You win!")
+    elif user_choice == 0 and computer_choice == 2:
+        print("You win!")
+    elif user_choice == computer_choice:
+        print("It's a draw.")
+    else:
+        print("You lose.")
