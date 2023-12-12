ecb1f3de1043e2333bef30c7f2139b495c929aad
tuyojr <olutuyod@gmail.com>
2023-12-12 12:04:16 +0100

    bankers roulette

--- 04_randomisation_and_lists/banker_roulette.py
+++ 04_randomisation_and_lists/banker_roulette.py
@@ -5,4 +5,5 @@ names = names_string.split(", ")
 
 number_of_names = len(names)
 index = random.randint(0, number_of_names-1)
-print(names[index])
+who_will_pay = names[index]
+print(f'{who_will_pay} is going to buy the meal today!')
