3969025b7502aaa81027c5851bae9e03fae8c0a5
tuyojr <olutuyod@gmail.com>
2023-12-19 10:27:17 +0100

    logo

---
+++ 09_dictionaries/art.py
@@ -0,0 +1,11 @@
+logo = '''
+88          88          88          88                        
+88          ""          88          88                        
+88                      88          88                        
+88,dPPYba,  88  ,adPPYb,88  ,adPPYb,88  ,adPPYba, 8b,dPPYba,  
+88P'    "8a 88 a8"    `Y88 a8"    `Y88 a8P_____88 88P'   "Y8  
+88       d8 88 8b       88 8b       88 8PP""""""" 88          
+88b,   ,a8" 88 "8a,   ,d88 "8a,   ,d88 "8b,   ,aa 88          
+8Y"Ybbd8"'  88  `"8bbdP"Y8  `"8bbdP"Y8  `"Ybbd8"' 88          
+
+'''
