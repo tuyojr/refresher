52e7f1cc1e399c1d376b17efb6f22ab38719b1b8
tuyojr <olutuyod@gmail.com>
2024-02-17 20:43:16 +0100

    logic for dealing card and calculating user current score

--- 11_blackjack_capstone_project/blackjack.py
+++ 11_blackjack_capstone_project/blackjack.py
@@ -1,4 +1,8 @@
 import random
+import os
+from art import logo
+
+clear = lambda: os.system('clear')
 
 ############### Our Blackjack House Rules #####################
 
@@ -12,5 +16,51 @@ cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 ## Cards are not removed from the deck as they are drawn.
 ## The computer is the dealer.
 
-user_cards = random.choices(cards, k=2)
-computer_cards = random.choices(cards, k=2)
+blackjack = 0
+
+option = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
+
+def deal(n=1):
+    return random.choices(cards, k=n)
+
+def card_score(list_of_cards: list):
+    score = sum(list_of_cards)
+    blackjack = 21
+    if score == blackjack:
+        return 0
+    if 11 in list_of_cards:
+        if score > blackjack:
+            list_of_cards.append(1)
+            list_of_cards.remove(11)
+    return score
+
+while option != 'n':
+    clear()
+    
+    print(logo)
+
+    user = deal(2)
+    computer = deal(2)
+
+    user_total = card_score(user)
+    computer_total = card_score(computer)
+
+    computer_status = f"\tComputer's first card: {computer[0]}"
+
+    print(f'\tYour cards: {user}, current score: {user_total}')
+    print(computer_status)
+
+    if user_total == blackjack or computer_total == blackjack or user_total > 21:
+        break
+    elif user_total < 21:
+        prompt = input("Type 'y' to get another card, type 'n' to pass: ")
+        if prompt == 'n':
+            break
+        elif prompt == 'y':
+            user.extend(deal())
+            user_total = card_score(user)
+            print(f'\tYour cards: {user}, current score: {user_total}')
+            if user_total > 21:
+                print("You went over. You lose 😭")
+
+    break
